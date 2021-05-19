41a4b34d5bdbe1c45b08e72e57cfa74530a0e3b3
Beccix <rennato.sg@gmail.com>
2021-05-19 13:54:07 -0300

    refazendo o layout todo

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-let btn = document.querySelectorAll('.btn-header');
-
-btn.forEach(function (btn) {
-    btn.addEventListener('click', function (btnHeader) {
-        console.log(btnHeader.currentTarget)
-    })
-})
