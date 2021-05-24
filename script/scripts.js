554aa7abb5600700676eb554059f27d3c9ee871b
Beccix <rennato.sg@gmail.com>
2021-05-24 13:54:33 -0300

    header done

---
+++ script/scripts.js
@@ -0,0 +1,29 @@
+function carddrop1() {
+    cardOne = document.querySelector(".card-down1")
+    
+    if (cardOne.style.display == "none") {
+        cardOne.style.display = "flex";
+    } else {
+        cardOne.style.display = "none";
+    }
+}
+
+function carddrop2() {
+    cardTwo = document.querySelector(".card-down2")
+    
+    if (cardTwo.style.display == "none") {
+        cardTwo.style.display = "flex";
+    } else {
+        cardTwo.style.display = "none";
+    }
+}
+
+function carddrop3() {
+    cardThree = document.querySelector(".card-down3")
+    
+    if (cardThree.style.display == "none") {
+        cardThree.style.display = "flex";
+    } else {
+        cardThree.style.display = "none";
+    }
+}
